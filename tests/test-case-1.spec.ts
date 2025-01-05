49ec75dad631ff7d32568b7530d01801cd783c27
AbdellahBellakrim <bellakrim2032@gmail.com>
2025-01-05 19:53:40 +0100

    Add initial README, update .gitignore, initiate user registration test, and remove example test case

---
+++ tests/test-case-1.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+test("Test Case 1: Register User", async ({ page }) => {
+  // Step 2: Navigate to the homepage
+  await page.goto("http://automationexercise.com");
+});
